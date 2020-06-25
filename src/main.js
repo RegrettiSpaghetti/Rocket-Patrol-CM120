d49e0e741f8846e8bfa0aa168308c141c878e1ca
Ethan Michaels <esmichae@ucsc.edu>
2020-06-25 14:30:29 -0700

    Initial Commit -- Directory Structure

---
+++ src/main.js
@@ -0,0 +1,9 @@
+//create game configuration obj
+let config = {
+    type: Phaser.CANVAS,
+    width: 640,
+    height: 480, 
+}
+
+//create main game object (Octopus head)
+let game = new Phaser.Game(config);
