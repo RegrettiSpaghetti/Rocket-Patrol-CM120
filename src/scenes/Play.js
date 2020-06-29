f1f6b2290e05a085f4fdb3b72119e7ad1bfe5abd
Ethan Michaels <esmichae@ucsc.edu>
2020-06-28 18:47:31 -0700

    Fixed a UI error

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -23,7 +23,7 @@ class Play extends Phaser.Scene {
         this.add.rectangle(5, 5, 630, 32, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(5, 443, 630, 32, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(5, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(603, 5, 32, 435, 0xFFFFFF).setOrigin(0, 0);
+        this.add.rectangle(603, 5, 32, 455, 0xFFFFFF).setOrigin(0, 0);
         // green UI background
         this.add.rectangle(37, 42, 566, 64, 0x00FF00).setOrigin(0, 0);
 
